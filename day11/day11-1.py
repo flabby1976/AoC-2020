db8bcc07d3d18878d238ca98f9397e6a6f13f317
flabby1976@gmail.com <e97vh9vx>
2020-12-11 13:16:42 -0500

    Added day 11

---
+++ day11/day11-1.py
@@ -0,0 +1,62 @@
+
+def disp_seats():
+    for r in seats:
+        print(r)
+    print("\n")
+
+def get_seat(r,c):
+    if r < 0 or c < 0:
+        return ""
+    try:
+        s = seats[r][c]
+    except IndexError:
+        s = ""
+    return s
+
+def adjacent_seats_occupied(row, col):
+    t = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+    adj=0
+    for r in t:
+        if get_seat(row+r[0],col+r[1]) == "#":
+            adj += 1
+    return adj
+
+with open('input.txt') as f:
+    seats = [[k for k in d] for d in f.read().split('\n')]
+
+seat_rows = len(seats)
+seats_per_row = len(seats[0])
+
+stable = False
+while not stable:
+    new_seats = []
+    for seat_row in range(seat_rows):
+        new_seats.append([" "]*seats_per_row)
+        for seat in range(seats_per_row):
+            adj = adjacent_seats_occupied(seat_row,seat)
+
+            if seats[seat_row][seat] == "L" and adj == 0:
+                    new_seats[seat_row][seat] = "#"
+
+            elif seats[seat_row][seat] == "#" and adj > 3:
+                    new_seats[seat_row][seat] = "L"
+
+            else:
+                new_seats[seat_row][seat] = seats[seat_row][seat]
+
+    if not new_seats == seats:
+        seats = new_seats
+    else:
+        stable = True
+
+disp_seats()
+
+c=0
+for r in seats:
+    c += r.count("#")
+print(c)
+
+
+            
+
+
