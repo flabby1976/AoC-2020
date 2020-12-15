704c02dc986750f61a57b60e51eff6c42faec344
flabby1976@gmail.com <e97vh9vx>
2020-12-15 10:26:00 -0500

    added timing

--- day15/day15-2.py
+++ day15/day15-2.py
@@ -4,25 +4,21 @@ start_time = time.time()
 start_list = [11,18,0,20,1,7,16]
 end_num = 30000000
 
-#use a dict to store the last positions of the numbers
-num_list = {}
-
-# store the start list first
+# Use a dict to store the last positions of the numbers.
+# Store the start list first
 s = len(start_list)
-for i in range(s-1):
-    num_list[start_list[i]] = i+1
+num_list = {start_list[i] : i+1 for i in range(s-1)}
 
-last_number = start_list[-1]
-for i in range(s+1, end_num+1):
+number = start_list[-1]
+for i in range(s, end_num):
 
-    if last_number not in num_list:
+    if number not in num_list:
         next_number = 0
     else:
-        next_number = (i - 1) - num_list[last_number]
+        next_number = i - num_list[number]
  
-    num_list[last_number] = i-1
- 
-    last_number = next_number
+    num_list[number] = i
+    number = next_number
 
-print("The {}th number spoken is {}".format(end_num, last_number))
+print("The {}th number spoken is {}".format(end_num, number))
 print("--- {} seconds ---".format(time.time() - start_time))
