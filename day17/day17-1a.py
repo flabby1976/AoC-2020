97b058c105fea82fa47e6e54d29412471a6634f5
flabby1976@gmail.com <e97vh9vx>
2020-12-17 12:34:54 -0500

    added day 17

---
+++ day17/day17-1a.py
@@ -0,0 +1,34 @@
+deltas = [-1,0,1]
+neighbours = [(x, y, z) for x in deltas for y in deltas for z in deltas]
+neighbours.remove((0,0,0))
+       
+def get_neighbours(cube):
+    (x, y, z) = cube
+    return set((x+dx, y+dy, z+dz) for (dx, dy, dz) in neighbours)
+
+def next_active(current_active):
+    next = set()
+    for active_cube in current_active:
+        nearest = get_neighbours(active_cube)
+        if len(nearest.intersection(current_active)) in [2,3]:
+            next.add(active_cube)
+        for n in nearest:
+            if n not in current_active:
+                nn = get_neighbours(n)
+                if len(nn.intersection(current_active)) == 3:
+                    next.add(n)
+    return next
+
+current_active = set()
+with open('input.txt') as f:
+    inp = [d for d in f.read().split('\n')]
+for y, line in enumerate(inp):
+    for x, ch in enumerate(line):
+        if ch == "#":
+            current_active.add((x,y,0))
+
+for cycle in range(6):
+    current_active = next_active(current_active)
+
+    
+print(len(current_active))
